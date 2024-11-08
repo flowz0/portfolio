29888ef1acd5e21c0fffd204febbc5dffc694156
flowz0 <billylflowers@gmail.com>
2024-11-08 15:23:10 -0800

    projects component setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "./_components/Hero";
+import Projects from "./_components/Projects";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Hero />
-    </main>
+      <Projects />
+    </>
   );
 }
