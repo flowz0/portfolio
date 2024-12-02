21b538e9fe8f734f29e1a4835b665f7819d67e39
flowz0 <billylflowers@gmail.com>
2024-12-01 22:06:00 -0800

    added full width to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ interface PrimaryProp {
 
 export default function Footer({ isPrimary = false }: PrimaryProp) {
     return (
-        <div className={isPrimary ? "bg-neutral-900 text-center" : "bg-neutral-950 text-center"}>
+        <div className={isPrimary ? "w-full text-center bg-neutral-900" : "w-full text-center bg-neutral-950"}>
             <p className="py-6 text-sm/none font-light tracking-widest text-neutral-300">
                 Powered by {' '}
                 <Link href="https://www.linkedin.com/in/billyflowers/" className="text-orange-500" isExternal>flowz0</Link>
