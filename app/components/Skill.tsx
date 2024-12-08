57a9d98d8653edc56f80f5908ac716f7431e3226
flowz0 <billylflowers@gmail.com>
2024-12-07 21:58:13 -0800

    Improved Skill.tsx

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -7,7 +7,7 @@ interface SkillProps {
 
 const Skill: React.FC<SkillProps> = ({icon: Icon, name}) => {
     return (
-        <div className="py-3 px-5 rounded-lg flex items-center gap-x-4 bg-neutral-800">
+        <div className="py-3 px-4 rounded-lg flex items-center gap-x-2.5 bg-neutral-800">
             <Icon size={20} />
             <p>{name}</p>
         </div>
