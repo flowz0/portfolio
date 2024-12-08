5a4e1884eb3ab125119334142a2af8c55c9ef4be
flowz0 <billylflowers@gmail.com>
2024-12-07 21:57:43 -0800

    Improved Project.tsx

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -14,21 +14,21 @@ interface ProjectProps {
 
 export default function Project({ img = TestImage, title, desc, site, code }: ProjectProps) {
     return (
-        <div className='rounded-lg h-full max-w-xs sm:max-w-sm bg-neutral-800'>
+        <div className='rounded-lg h-full max-w-xs bg-neutral-900 sm:max-w-sm sm:bg-neutral-800'>
             <Image
                 src={img}
                 alt={`${title} project image`}
                 quality={90}
-                className='object-cover h-56 w-full rounded-t-lg'
+                className='object-cover h-56 w-full rounded-lg sm:h-56 sm:rounded-t-lg sm:rounded-b-none'
             />
-            <div className='px-4 py-3.5 flex flex-col justify-between h-fit sm:px-4 sm:py-4'>
+            <div className='mt-2 flex flex-col justify-between h-fit sm:mt-0 sm:p-5'>
                 <div>
-                    <h4 className='text-2xl/none font-semibold tracking-wide'>{title}</h4>
-                    <p className='mt-3 leading-6 tracking-wider text-neutral-300'>{desc}</p>
+                    <h4 className='text-3xl/8 tracking-normal text-neutral-200'>{title}</h4>
+                    <p className='mt-2 text-base/6 tracking-wide text-neutral-400'>{desc}</p>
                 </div>
                 <div className="flex justify-between items-baseline">
-                    <Button className="mt-6" text="Show Details" />
-                    <div className="flex gap-x-3">
+                    <Button className="mt-4" text="Show Details" />
+                    <div className="flex gap-x-2">
                         {site && (
                             <Link href={site} target="_blank" className="text-xl/none text-neutral-400 hover:text-orange-500">
                                 <FaExternalLinkAlt />
