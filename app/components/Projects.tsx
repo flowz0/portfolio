a7dba1bb7d4f444ef324bd75139374e0410de21e
flowz0 <billylflowers@gmail.com>
2024-12-07 21:58:00 -0800

    Improved Projects.tsx

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -5,10 +5,10 @@ import ProjectDDDC from "@/assets/project-dddc.png";
 
 export default function Projects() {
     return (
-        <div id="projects" className='py-16 flex flex-col items-center bg-neutral-900'>
-            <h2 className="text-sm/none font-light text-orange-500">Projects</h2>
-            <h3 className="text-4xl/none font-semibold mt-3.5">My Work</h3>
-            <div className="mt-8 mx-4 flex flex-wrap justify-center gap-4">
+        <div id="projects" className='px-6 py-16 flex flex-col items-center bg-neutral-900'>
+            <h2 className="text-base/none font-light text-orange-500 sm:text-sm/none">Projects</h2>
+            <h3 className="mt-4 text-4xl/none font-semibold text-neutral-200 sm:text-5xl/none">My Work</h3>
+            <div className="mt-6 flex flex-wrap justify-center gap-y-6 gap-x-4 sm:mx-4">
 
                 <Project
                     img={ProjectDDDC}
