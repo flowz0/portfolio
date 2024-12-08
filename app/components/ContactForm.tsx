a9faaa68b7cda1a869c422eb4065d0aa4035807c
flowz0 <billylflowers@gmail.com>
2024-12-07 21:56:31 -0800

    Improved ContactForm.tsx

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -2,26 +2,29 @@ import Button from "./Button";
 
 export default function ContactForm() {
     return (
-        <form className="px-4 mt-8 mb-4 w-full flex flex-col max-w-xl">
-            <div className="sm:grid sm:grid-cols-2 sm:gap-x-6">
+        <form className="mt-6 w-full flex flex-col gap-y-2.5">
+            <div className="flex flex-col gap-y-2.5 sm:grid sm:grid-cols-2 sm:gap-x-4">
                 <div className="flex flex-col">
-                    <label htmlFor="name" className="text-neutral-300">Name</label>
-                    <input id="name" type="text" className="mt-2 py-2 px-1.5 rounded bg-neutral-800 text-neutral-300 placeholder:text-neutral-500" placeholder="Enter name" />
+                    <label htmlFor="name" className="pl-2 text-neutral-400">Name</label>
+                    <input id="name" type="text" className="mt-1 py-1.5 px-2 rounded-lg bg-neutral-800 text-neutral-400 placeholder:text-neutral-600" placeholder="Enter name" />
                 </div>
 
                 <div className="flex flex-col">
-                    <label htmlFor="phone" className="mt-5 sm:mt-0 text-neutral-300">Phone</label>
-                    <input id="phone" type="text" className="mt-2 py-2 px-1.5 rounded bg-neutral-800 text-neutral-300 placeholder:text-neutral-500" placeholder="Enter phone" />
+                    <label htmlFor="phone" className="sm:mt-0 pl-2 text-neutral-400">Phone</label>
+                    <input id="phone" type="text" className="mt-1 py-1.5 px-2 rounded-lg bg-neutral-800 text-neutral-400 placeholder:text-neutral-600" placeholder="Enter phone" />
                 </div>
 
             </div>
-            <label htmlFor="email" className="mt-5 text-neutral-300">Email</label>
-            <input id="email" type="email" className="mt-2 py-2 px-1.5 rounded bg-neutral-800 text-neutral-300 placeholder:text-neutral-500" placeholder="Enter email" />
+            <div className="flex flex-col">
+                <label htmlFor="email" className="pl-2 text-neutral-400">Email</label>
+                <input id="email" type="email" className="mt-1 py-1.5 px-2 rounded-lg bg-neutral-800 text-neutral-400 placeholder:text-neutral-600" placeholder="Enter email" />
+            </div>
+            <div className="flex flex-col">
+                <label htmlFor="message" className="pl-2 text-neutral-400">Message</label>
+                <textarea id="message" rows={4} className="mt-1 py-1.5 px-2 rounded-lg resize-none bg-neutral-800 text-neutral-400 placeholder:text-neutral-600" placeholder="Enter message" />
+            </div>
 
-            <label htmlFor="message" className="mt-5 text-neutral-300">Message</label>
-            <textarea id="message" rows={4} className="mt-2 py-2 px-1.5 rounded resize-none bg-neutral-800 text-neutral-300 placeholder:text-neutral-500" placeholder="Enter message" />
-
-            <Button className="mt-4 self-end" text="Message" submit />
+            <Button className="self-end" text="Message" submit />
         </form>
     );
 }
