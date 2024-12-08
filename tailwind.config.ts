63ea30dbadb4e0b971852371fe9d3cbd297090d4
flowz0 <billylflowers@gmail.com>
2024-12-07 22:16:56 -0800

    Removed multi component config and added single component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    './node_modules/@nextui-org/theme/dist/components/(navbar|image).js'
+    './node_modules/@nextui-org/theme/dist/components/navbar.js'
+    // './node_modules/@nextui-org/theme/dist/components/(navbar|image).js'
   ],
   theme: {
     extend: {
