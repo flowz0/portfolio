94f8de79e9c6df60ce398d0369fb2b2830c205e5
flowz0 <billylflowers@gmail.com>
2024-12-07 21:56:09 -0800

    Update and improved Contact.tsx

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -5,12 +5,14 @@ import ContactForm from "./ContactForm";
 export default function Contact() {
     return (
         <div>
-            <div className="pt-16 flex flex-col items-center justify-center bg-neutral-900">
-                <div className="mx-4 py-4 w-full flex flex-col items-center">
-                    <h2 className="text-sm/none font-light mt-6 text-orange-500">Contact</h2>
-                    <h3 className="text-4xl/none font-semibold mt-3.5 sm:text-5xl">Let's Work</h3>
+            <div className="px-6 py-16 flex flex-col items-center justify-center bg-neutral-900">
+                <div className="w-full flex flex-col items-center">
+                    <h2 className="text-base/none font-light text-orange-500 sm:text-sm/none">Contact</h2>
+                    <h3 className="mt-4 text-4xl/none font-semibold text-neutral-200 sm:text-5xl/none">Let's Work</h3>
                 </div>
-                <ContactForm />
+                <div className="w-full max-w-xl">
+                    <ContactForm />
+                </div>
             </div>
             <Divider className="w-full" />
             <Footer />
