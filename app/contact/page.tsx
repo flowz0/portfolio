d1c92cc558c31bb87db5f19c824ab38fa3fbcc92
flowz0 <billylflowers@gmail.com>
2024-12-07 22:24:06 -0800

    Updated app/contact page Decreased top & bottom padding

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,7 +11,7 @@ export default function page() {
                 <h3 className="mt-4 text-4xl/none text-neutral-200 font-semibold sm:text-5xl/none">Let's Work</h3>
             </div>
             <Divider className="w-full" flipped />
-            <div className="px-6 py-4 flex flex-col bg-neutral-900 sm:flex-row sm:justify-center sm:gap-x-6 sm:py-20">
+            <div className="px-6 py-4 flex flex-col bg-neutral-900 sm:flex-row sm:justify-center sm:gap-x-6 sm:py-16">
                 <div className='pl-2 sm:pl-0'>
                     <ContactSocials className="sm:flex-col sm:gap-y-2.5 sm:mt-6" />
                 </div>
