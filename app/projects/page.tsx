9385c5b4e798d885ba5df062b74f0ff035d97c9f
flowz0 <billylflowers@gmail.com>
2024-12-08 21:37:43 -0800

    Improved app/projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,16 +1,13 @@
 import Project from "../components/Project";
-import Footer from "../components/Footer";
-import Divider from "../components/Divider";
 
 export default function page() {
   return (
-    <>
-      <div className="pt-16 pb-6 flex flex-col items-center bg-neutral-950">
+    <div className="m-4 flex flex-col gap-y-4">
+      <div className="mt-16 py-8 rounded-lg flex flex-col items-center bg-neutral-900">
         <h2 className="text-base/none font-light text-orange-500 sm:text-sm/none">Projects</h2>
-        <h3 className="mt-4 text-4xl/none text-neutral-200 font-semibold sm:text-5xl/none">My Work</h3>
+        <h3 className="mt-4 text-4xl/none text-neutral-300 font-semibold sm:text-5xl/none">My Work</h3>
       </div>
-      <Divider className="w-full" flipped />
-      <div className='px-6 py-16 flex flex-col items-center bg-neutral-900'>
+      <div className='px-4 py-6 rounded-lg flex flex-col items-center bg-neutral-900 sm:py-10'>
         <div className="flex flex-wrap justify-center gap-6 sm:gap-4 sm:max-w-7xl">
           <Project
             // img={}
@@ -49,9 +46,7 @@ export default function page() {
           />
         </div>
       </div>
-      <Divider className="w-full" />
-      <Footer />
 
-    </>
+    </div>
   )
 }
