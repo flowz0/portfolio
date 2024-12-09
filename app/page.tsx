4696c605d8f94a024c41a672068c625938349975
flowz0 <billylflowers@gmail.com>
2024-12-08 21:35:56 -0800

    Removed the Divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 import Contact from "./components/Contact";
-import Divider from "./components/Divider";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 export default function Home() {
   return (
-    <>
+    <div className="p-4 flex flex-col gap-y-4">
       <Hero />
-      <Divider flipped />
       <Skills />
       <Projects />
       <Contact />
-    </>
+    </div>
   );
 }
