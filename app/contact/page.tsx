a4ac63aa1d00117e5de25fe5fa62eaa5eddbc5bb
flowz0 <billylflowers@gmail.com>
2024-12-08 21:37:35 -0800

    Improved app/contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,26 +1,21 @@
 import ContactForm from '../components/ContactForm'
 import ContactSocials from '../components/ContactSocials'
-import Divider from '../components/Divider'
-import Footer from '../components/Footer'
 
 export default function page() {
     return (
-        <>
-            <div className="pt-16 pb-6 px-6 flex flex-col items-center bg-neutral-950">
+        <div className="m-4 flex flex-col gap-y-4">
+            <div className="mt-16 py-8 rounded-lg flex flex-col items-center bg-neutral-900">
                 <h2 className="text-base/none font-light text-orange-500 sm:text-sm/none">Contact</h2>
-                <h3 className="mt-4 text-4xl/none text-neutral-200 font-semibold sm:text-5xl/none">Let's Work</h3>
+                <h3 className="mt-4 text-4xl/none text-neutral-300 font-semibold sm:text-5xl/none">Let's Work</h3>
             </div>
-            <Divider className="w-full" flipped />
-            <div className="px-6 py-4 flex flex-col bg-neutral-900 sm:flex-row sm:justify-center sm:gap-x-6 sm:py-16">
-                <div className='pl-2 sm:pl-0'>
-                    <ContactSocials className="sm:flex-col sm:gap-y-2.5 sm:mt-6" />
+            <div className="px-4 py-6 rounded-lg flex flex-col bg-neutral-900 sm:flex-row sm:justify-center sm:gap-x-6 sm:py-10">
+                <div className="pl-2 flex sm:order-2 justify-center sm:pl-0 sm:flex-none">
+                    <ContactSocials className="sm:flex-col sm:gap-y-3 sm:mt-2" />
                 </div>
-                <div className='w-full max-w-xl'>
+                <div className="w-full max-w-xl sm:order-1">
                     <ContactForm />
                 </div>
             </div>
-            <Divider className="w-full" />
-            <Footer />
-        </>
+        </div> 
     )
 }
