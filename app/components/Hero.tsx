97f03cc3f89a13ae160f6234a5497dfb8ee26e62
flowz0 <billylflowers@gmail.com>
2024-12-08 21:36:33 -0800

    Improved Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,14 +3,14 @@ import ContactSocials from "./ContactSocials";
 
 export default function Hero() {
     return (
-        <main id="about" className="px-6 py-16 flex flex-col justify-center items-center">
-            <h1 className="text-4xl/none text-neutral-200 font-semibold sm:text-6xl/none">
+        <main id="about" className="mt-16 px-6 py-20 rounded-lg flex flex-col justify-center items-center bg-neutral-900 sm:py-24">
+            <h1 className="text-4xl/none text-neutral-300 font-semibold sm:text-6xl/none">
                 Billy Flowers
             </h1>
-            <p className="mt-2 text-base/none font-normal tracking-wide text-orange-500 sm:text-lg/none">
+            <p className="mt-4 text-base/none font-normal tracking-wide text-orange-500 sm:text-lg/none">
                 Software Engineer
             </p>
-            <ContactSocials className="mt-4" />
+            <ContactSocials className="mt-6" />
             <Button className="mt-4" text="Download CV" />
         </main>
     );
