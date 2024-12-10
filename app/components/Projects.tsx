670894c6bf15341272f6766daa578bf8aa83de95
flowz0 <billylflowers@gmail.com>
2024-12-10 08:31:31 -0800

    Mapped project data and sorted projects by date (most recent on top)

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,36 +1,32 @@
 import Link from "next/link";
 import Project from "./Project";
 
-import ProjectDDDC from "@/assets/project-dddc.png";
+import projectsData from "../data/projectsData";
 
 export default function Projects() {
+    // Sort projects by date (most recent first)
+    const sortedProjects = projectsData.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+    // For landing page, get only the three most recent projects
+    const recentProjects = sortedProjects.slice(0, 3);
+
     return (
         <div id="projects" className='px-4 py-16 rounded-lg flex flex-col items-center bg-neutral-900'>
             <h2 className="text-base/none font-light text-orange-500 sm:text-sm/none">Projects</h2>
             <h3 className="mt-4 text-4xl/none font-semibold text-neutral-300 sm:text-5xl/none">My Work</h3>
-            <div className="mt-6 flex flex-wrap justify-center gap-y-6 gap-x-4 sm:mx-4">
-
-                <Project
-                    img={ProjectDDDC}
-                    title="Duct Daddy Duct Cleaning"
-                    desc="HVAC cleaning service website built with Next.js, TypeScript, SendGrid, and TailwindCSS..."
-                    site="https://www.ductdaddykc.com/"
-                    code="https://github.com/flowz0/dddc"
-                />
-                <Project
-                    // img={ProjectDDDC2}
-                    title="Project Title"
-                    desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo nostrum..."
-                    site="https://bflows.dev"
-                    code="https://github.com/bflows/portfolio"
-                />
-                <Project
-                    // img={}
-                    title="Project Title"
-                    desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo nostrum..."
-                    site="https://bflows.dev"
-                    code="https://github.com/bflows/portfolio"
-                />
+            <div className="mt-6 flex flex-wrap justify-center gap-y-8 gap-x-4 sm:mx-4">
+        
+                {recentProjects.map((project, index) => (
+                    <Project
+                        key={index}
+                        img={project.img}
+                        title={project.title}
+                        desc={project.desc}
+                        site={project.site}
+                        code={project.code}
+                        date={project.date}
+                    />
+                ))}
                 
 
             </div>
