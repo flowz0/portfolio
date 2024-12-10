ab091037c925ea873cc5007745dde083f38a582a
flowz0 <billylflowers@gmail.com>
2024-12-10 08:31:47 -0800

    Updated RoleTypeWriter.tsx words

--- app/components/RoleTypeWriter.tsx
+++ app/components/RoleTypeWriter.tsx
@@ -5,7 +5,7 @@ import { Typewriter } from "react-simple-typewriter";
 export default function RoleTypeWriter() {
   return (
     <Typewriter 
-      words={["Full-Stack Developer", "Designer", "Software Engineer"]}
+      words={["Full-Stack Web Developer", "Computer Programmer", "Software Engineer"]}
       loop={2}
     />
   )
