49236532923a6fce8341fecb1a7000221ff66b19
flowz0 <billylflowers@gmail.com>
2024-12-14 00:23:59 -0800

    Implemented SendGrid Email API for ContactForm submission

---
+++ app/actions/index.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { sendEmail } from "../lib/sendgrid";
+
+export const sendContactEmailAction = async (formData: FormData) => {
+  try {
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const message = formData.get("message") as string;
+
+    await sendEmail({
+      to: "billylflowers@gmail.com",
+      templateName: "ContactSubmission",
+      dynamicTemplateData: {
+        name,
+        email,
+        message,
+      },
+    });
+
+    return { errorMessage: null };
+  } catch (error) {
+    console.error(error);
+    return { errorMessage: "Something went wrong" };
+  }
+};
