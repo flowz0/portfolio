db7f85013c01ab833e7d26163f93ec053743ad9a
flowz0 <billylflowers@gmail.com>
2024-12-14 16:11:14 -0800

    Updated Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,9 +7,9 @@ interface PrimaryProp {
 export default function Footer({ isPrimary = false }: PrimaryProp) {
     return (
         <div className={isPrimary ? "w-full text-center bg-neutral-900" : "w-full text-center bg-neutral-950"}>
-            <p className="pt-6 pb-12 text-sm/none font-light tracking-widest text-neutral-300">
+            <p className="pt-6 pb-12 text-sm/none tracking-widest text-neutral-400">
                 © 2025 {' '}
-                <Link href="https://www.bflows.dev" className="font-bold text-orange-600 hover:text-orange-700">flowz0</Link>
+                <Link href="https://www.bflows.dev" className=" text-orange-600 hover:text-orange-700">flowz0</Link>
             </p>
         </div>
     );
