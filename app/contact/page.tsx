6ba9a9fd260a20c75ec9a228affe3c57c1101c63
flowz0 <billylflowers@gmail.com>
2024-12-16 22:13:55 -0800

    Increased padding

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,12 +3,12 @@ import ContactSocials from '../components/ContactSocials'
 
 export default function page() {
     return (
-        <div className="m-4 flex flex-col gap-y-4">
-            <div className="mt-16 py-8 rounded-lg flex flex-col items-center bg-neutral-900">
+        <div className="m-4 flex flex-col gap-y-4 bg-neutral-950">
+            <div className="mt-16 py-8 rounded-lg flex flex-col items-center bg-neutral-900 sm:py-14">
                 <h2 className="text-sm/none text-orange-500">Contact</h2>
                 <h3 className="mt-4 text-4xl/none text-neutral-300 font-semibold sm:text-5xl/none">Let&apos;s Work</h3>
             </div>
-            <div className="px-4 py-6 rounded-lg flex flex-col bg-neutral-900 sm:flex-row sm:justify-center sm:gap-x-6 sm:py-10">
+            <div className="px-4 py-6 rounded-lg flex flex-col bg-neutral-900 sm:flex-row sm:justify-center sm:gap-x-6 sm:py-20">
                 <div className="pl-2 flex sm:order-2 justify-center sm:pl-0 sm:flex-none">
                     <ContactSocials className="sm:flex-col sm:gap-y-3 sm:mt-2" />
                 </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,12 +3,12 @@ import projectsData from "../data/projectsData";
 
 export default function page() {
   return (
-    <div className="m-4 flex flex-col gap-y-4">
-      <div className="mt-16 py-8 rounded-lg flex flex-col items-center bg-neutral-900">
+    <div className="m-4 flex flex-col gap-y-4 bg-neutral-950">
+      <div className="mt-16 py-8 rounded-lg flex flex-col items-center bg-neutral-900 sm:py-14">
         <h2 className="text-sm/none text-orange-500">Projects</h2>
         <h3 className="mt-4 text-4xl/none text-neutral-300 font-semibold sm:text-5xl/none">My Work</h3>
       </div>
-      <div className='px-4 py-6 rounded-lg flex flex-col items-center bg-neutral-900 sm:py-10'>
+      <div className='px-4 py-6 rounded-lg flex flex-col items-center bg-neutral-900 sm:py-20'>
         <div className="flex flex-wrap justify-center gap-8 sm:gap-4 sm:max-w-7xl">
           {projectsData.map((project, index) => (
             <Project
