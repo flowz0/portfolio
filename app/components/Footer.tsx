154524e37346dcdbf453522e402b64d48c100872
flowz0 <billylflowers@gmail.com>
2024-12-16 22:12:26 -0800

    Increased Footer padding

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,7 @@ interface PrimaryProp {
 export default function Footer({ isPrimary = false }: PrimaryProp) {
     return (
         <div className={isPrimary ? "w-full text-center bg-neutral-900" : "w-full text-center bg-neutral-950"}>
-            <p className="pt-6 pb-12 text-sm/none font-light tracking-widest text-neutral-300">
+            <p className="pt-8 pb-16 text-sm/none font-light tracking-widest text-neutral-300">
                 © 2025 {' '}
                 <Link href="https://www.bflows.dev" className="font-bold text-orange-600 hover:text-orange-700">flowz0</Link>
             </p>
