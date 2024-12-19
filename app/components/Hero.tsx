5517b7a3ec9afe4f5a44da5e41ff56b01a91409a
flowz0 <billylflowers@gmail.com>
2024-12-19 13:15:40 -0800

    Implement CV

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,7 @@
 import ContactSocials from "./ContactSocials";
 import RoleTypeWriter from "./RoleTypeWriter";
 import { noto_sans, poppins } from "../fonts";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -14,11 +15,19 @@ export default function Hero() {
         Billy Flowers
       </h1>
       <p
-        className={`${noto_sans.className} mt-4 text-base/none font-light tracking-wide text-orange-500 sm:text-lg/none`}
+        className={`${noto_sans.className} mt-5 text-base/none font-light tracking-wide text-orange-500 sm:text-lg/none`}
       >
         I&apos;m a <RoleTypeWriter />
       </p>
-      <ContactSocials className="mt-6" />
+      <ContactSocials className="mt-5" />
+      <Link href={`/billys-resume.pdf`} target="_blank">
+        <button
+          type="button"
+          className={`${noto_sans.className} mt-6 flex py-2 px-3.5 rounded-lg bg-neutral-700 text-neutral-400 hover:bg-neutral-800 active:scale-95`}
+        >
+          View CV
+        </button>
+      </Link>
     </main>
   );
 }
