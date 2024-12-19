9eaf681f8d1c1e8d8855d11e8a755efc1b2a97b0
flowz0 <billylflowers@gmail.com>
2024-12-19 05:15:17 -0800

    Update Skill.tsx Added hover scale on component

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -7,7 +7,7 @@ interface SkillProps {
 
 const Skill: React.FC<SkillProps> = ({icon: Icon, name}) => {
     return (
-        <div className="py-3 px-4 rounded-lg flex items-center gap-x-2 bg-neutral-800 text-neutral-400 hover:outline-none hover:ring-1 hover:ring-orange-600">
+        <div className="py-3 px-4 rounded-lg flex items-center gap-x-2 bg-neutral-800 text-neutral-400 hover:outline-none hover:ring-1 hover:ring-orange-600 hover:scale-110">
             <Icon size={20} />
             <p>{name}</p>
         </div>
