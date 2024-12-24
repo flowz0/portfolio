bb2e2d6f86c60134ab3af74caf093caa02ad3fcc
flowz0 <billylflowers@gmail.com>
2024-12-24 00:08:37 -0800

    Created not-found.tsx Page not found

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { noto_sans } from "./fonts";
+
+export default function NotFoundPage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-[calc(100vh-160px)] px-6 text-center bg-neutral-950">
+      <h2
+        className={`${noto_sans.className} text-4xl font-semibold text-neutral-300 sm:text-5xl`}
+      >
+        <span className="text-orange-500">404</span> - Page Not Found
+      </h2>
+      <p className="mt-6 text-neutral-300">
+        Sorry, the page you&apos;re looking for doesn&apos;t exist.
+      </p>
+      <Link
+        href="/"
+        className="mt-8 px-6 py-2 bg-orange-500 text-white rounded-full hover:bg-orange-600 transition-colors"
+      >
+        Return to Home
+      </Link>
+    </main>
+  );
+}
