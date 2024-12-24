22cc60fe051ae42df7016d2a4b566492d227d612
flowz0 <billylflowers@gmail.com>
2024-12-24 05:02:39 -0800

    Improved Skill.tsx

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -8,7 +8,7 @@ interface SkillProps {
 
 const Skill: React.FC<SkillProps> = ({icon: Icon, name}) => {
     return (
-        <p className={`${noto_sans.className} py-3 px-4 rounded-lg flex items-center gap-x-2 bg-neutral-900 text-neutral-400 hover:-translate-y-1`}>
+        <p className={`${noto_sans.className} py-3 px-4 rounded-lg flex items-center gap-x-2 transition ease-in-out delay-75 duration-300 bg-neutral-900 text-neutral-400 hover:ring-1 hover:ring-teal-400 hover:scale-105 hover:-translate-y-1 active:scale-95`}>
             <Icon size={20} />
             <span className="cursor-default">{name}</span>
         </p>
