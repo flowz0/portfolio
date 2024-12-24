0b6261923cb09b97b8059167efb828f77c2c148f
flowz0 <billylflowers@gmail.com>
2024-12-24 04:58:38 -0800

    Improved Button.tsx Added animated variant

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -6,20 +6,21 @@ interface ButtonProps {
     text: string;
     submit?: boolean;
     className?: string;
-    variant?: 'primary' | 'secondary';
+    variant?: 'primary' | 'secondary' | 'animated';
 }
 
 export default function Button({ text, className, submit = false, variant = 'primary' }: ButtonProps) {
     const baseClass = "flex py-2 px-3.5 rounded-lg"
     const variantClasses = {
-        primary: "bg-neutral-800 text-neutral-400 hover:bg-neutral-900",
-        secondary: "bg-neutral-300 text-neutral-700 hover:bg-neutral-400"
+        primary: "bg-gradient-to-r from-cyan-600 to-teal-700 text-neutral-300",
+        secondary: "bg-neutral-300 text-neutral-700 hover:bg-neutral-400",
+        animated: "bg-gradient-to-r from-cyan-600 to-teal-700 animate-background-pulse"
     }
 
     return (
         <button
             type={submit ? 'submit' : 'button'}
-            className={`${baseClass} ${variantClasses[variant]} ${className} ${noto_sans.className} active:scale-95`}
+            className={`${baseClass} ${variantClasses[variant]} ${className} ${noto_sans.className} transition ease-in-out delay-75 duration-300 hover:scale-105 hover:-translate-y-1 active:scale-95`}
         >
             {text}
         </button>
