657891112ea9d697d16365e5767af807ba6d9d77
flowz0 <billylflowers@gmail.com>
2024-12-24 05:05:00 -0800

    Optimized import and added an animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from "tailwindcss";
-const { nextui } = require("@nextui-org/theme");
+import { nextui } from "@nextui-org/theme";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    './node_modules/@nextui-org/theme/dist/components/navbar.js'
+    "./node_modules/@nextui-org/theme/dist/components/navbar.js",
     // './node_modules/@nextui-org/theme/dist/components/(navbar|image).js'
   ],
   theme: {
@@ -15,10 +15,17 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        "background-pulse": "pulse 2s ease-in-out infinite",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0.8" },
+        },
+      },
     },
   },
-  plugins: [
-    nextui()
-  ],
+  plugins: [nextui()],
 };
 export default config;
