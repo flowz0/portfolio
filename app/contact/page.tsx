fd7ae42b5aa5514ee8fb86ab094671eb214bc66d
flowz0 <billylflowers@gmail.com>
2024-12-24 05:03:08 -0800

    Improved contact/page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,8 +6,8 @@ export default function page() {
     return (
         <section className="min-h-[calc(100vh-160px)] max-w-xl mx-auto px-6 pt-20 flex flex-col w-full">
             <div className="flex flex-col items-center">
-                <h2 className={`${noto_sans.className} text-sm/none font-light text-orange-500`}>Contact</h2>
-                <h3 className={`${poppins.className} mt-4 text-4xl/none font-semibold text-neutral-300 sm:text-5xl/none`}>Let&apos;s Work</h3>
+                <h2 className={`${noto_sans.className} text-sm font-light bg-gradient-to-r from-cyan-500 to-teal-400 inline-block text-transparent bg-clip-text`}>Contact</h2>
+                <h3 className={`${poppins.className} mt-4 text-4xl font-semibold bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-5xl`}>Let&apos;s Work</h3>
             </div>
             <div className="mt-8">
                 <div className="flex justify-center">
