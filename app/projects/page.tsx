3d4f7c77d9fde88a2bb980f1cee50f2138270174
flowz0 <billylflowers@gmail.com>
2024-12-24 05:03:22 -0800

    Improved projects/page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,8 +6,8 @@ export default function page() {
   return (
     <div className="mx-6 pt-20 pb-16 flex flex-col items-center">
       <div className="flex flex-col items-center">
-        <h2 className={`${noto_sans.className} text-sm/none font-light text-orange-500`}>Projects</h2>
-        <h3 className={`${poppins.className} mt-4 text-4xl/none font-semibold text-neutral-300 sm:text-5xl/none`}>
+        <h2 className={`${noto_sans.className} text-sm font-light bg-gradient-to-r from-cyan-500 to-teal-400 inline-block text-transparent bg-clip-text`}>Projects</h2>
+        <h3 className={`${poppins.className} mt-4 text-4xl font-semibold bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-5xl`}>
           My Work
         </h3>
       </div>
