1c36be54889fd18f796e770873fd47790f6b53d9
flowz0 <billylflowers@gmail.com>
2024-12-25 23:34:36 -0800

    Line heights set Work in progress

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -36,14 +36,17 @@ export default async function ProjectDetails({
     <main className="mx-6 mt-20 max-w-2xl sm:mx-auto">
       <div className="flex flex-col items-center">
         <h2
-          className={`${noto_sans.className} text-sm font-light bg-gradient-to-r from-cyan-500 to-teal-400 inline-block text-transparent bg-clip-text`}
+          className={`${noto_sans.className} text-sm/5 font-light bg-gradient-to-r from-cyan-500 to-teal-400 inline-block text-transparent bg-clip-text`}
         >
           Details
         </h2>
         <h3
-          className={`${poppins.className} mt-4 text-4xl font-semibold bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-5xl/normal`}
+          className={`${poppins.className} mt-4 text-4xl/normal font-semibold bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-5xl/normal`}
         >
-          Project 00{project.id}
+          Project {" "}
+          <span className="bg-gradient-to-r from-cyan-500 to-teal-400 inline-block text-transparent bg-clip-text">
+            00{project.id}
+          </span>
         </h3>
       </div>
       <div className="mt-8 flex justify-between items-center sm:mt-12">
@@ -79,15 +82,15 @@ export default async function ProjectDetails({
         alt={`${project.title} project image`}
         className="mt-4 h-auto w-full rounded"
       />
+
       <h2
-        className={`${poppins.className} mt-4 text-3xl font-semibold bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-4xl/relaxed`}
+        className={`${poppins.className} mt-6 text-3xl/10 font-semibold bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-4xl/relaxed`}
       >
         {project.title}
       </h2>
-      <p className={`${noto_sans.className} mt-4 text-lg text-neutral-400`}>
+      <p className={`${noto_sans.className} mt-4 text-lg/8 text-neutral-400`}>
         {project.desc}
       </p>
-
       <ul className="mt-12 flex flex-col gap-y-12">
         {project.lessons && (
           <li>
