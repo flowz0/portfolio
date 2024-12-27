3f0682fd83c8dfd397d7e221a4e0ebd4d879a2eb
flowz0 <billylflowers@gmail.com>
2024-12-27 02:30:39 -0800

    Improved page.tsx (project/[id])

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaExternalLinkAlt, FaGithub, FaCalendarAlt } from "react-icons/fa";
 import projectsData from "../../data/projectsData";
-import { noto_sans, poppins } from "../../fonts";
+import { arimo, nunito } from "@/app/fonts";
 
 export async function generateStaticParams() {
   return projectsData.map((project) => ({
@@ -19,6 +19,8 @@ export default async function ProjectDetails({
   const { id } = await params;
   const projectId = parseInt(id);
   const project = projectsData.find((p) => p.id === projectId);
+  const iconClass =
+    "text-2xl text-neutral-500 transition ease-in-out delay-75 duration-300 hover:text-teal-500 hover:scale-105 active:scale-95";
 
   if (!project) {
     notFound();
@@ -33,94 +35,85 @@ export default async function ProjectDetails({
   };
 
   return (
-    <main className="mx-6 mt-20 max-w-2xl sm:mx-auto">
-      <div className="flex flex-col items-center">
-        <h2
-          className={`${noto_sans.className} text-sm/5 font-light bg-gradient-to-r from-cyan-500 to-teal-400 inline-block text-transparent bg-clip-text`}
+    <article className="mx-6 mt-12 py-12 flex flex-col items-center">
+      <section className="max-w-3xl">
+        <header className="flex flex-col items-center">
+          <h2
+            className={`${nunito.className} text-base leading-normal tracking-normal text-teal-400 sm:text-lg`}
+          >
+            Detail
+          </h2>
+          <h3
+            className={`${arimo.className} mt-2 leading-normal tracking-normal text-4xl font-bold text-neutral-200 sm:text-6xl`}
+          >
+            Project 00{project.id}
+          </h3>
+        </header>
+        <section className="mt-8 flex justify-between items-baseline text-neutral-400">
+          <p className="flex items-center">
+            <FaCalendarAlt className="mr-2" />
+            {formatDate(project.date)}
+          </p>
+          <section className="flex gap-4">
+            {project.site && (
+              <Link href={project.site} target="_blank" className={iconClass}>
+                <FaExternalLinkAlt />
+              </Link>
+            )}
+            {project.code && (
+              <Link href={project.code} target="_blank" className={iconClass}>
+                <FaGithub />
+              </Link>
+            )}
+          </section>
+        </section>
+        <Image
+          src={project.img}
+          alt={`${project.title} project image`}
+          className="mt-4 h-auto w-full rounded"
+          priority={true}
+        />
+        <h4
+          className={`${arimo.className} mt-6 leading-normal tracking-normal text-3xl font-bold text-neutral-300 sm:text-5xl`}
         >
-          Details
-        </h2>
-        <h3
-          className={`${poppins.className} mt-4 text-4xl/normal font-semibold bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-5xl/normal`}
-        >
-          Project {" "}
-          <span className="bg-gradient-to-r from-cyan-500 to-teal-400 inline-block text-transparent bg-clip-text">
-            00{project.id}
-          </span>
-        </h3>
-      </div>
-      <div className="mt-8 flex justify-between items-center sm:mt-12">
+          {project.title}
+        </h4>
         <p
-          className={`${noto_sans.className} flex items-center text-neutral-400`}
+          className={`${nunito.className} mt-4 text-base leading-normal tracking-normal text-neutral-400`}
         >
-          <FaCalendarAlt className="mr-2" />
-          {formatDate(project.date)}
+          {project.desc}
         </p>
-        <div className="flex gap-2 text-xl">
-          {project.site && (
-            <Link
-              href={project.site}
-              target="_blank"
-              className="transition ease-in-out delay-75 duration-300 text-neutral-500 hover:text-teal-500 hover:scale-105 hover:-translate-y-1 active:scale-95"
-            >
-              <FaExternalLinkAlt />
-            </Link>
+        <ul className="mt-12 flex flex-col gap-y-8">
+          {project.lessons && (
+            <li>
+              <h5
+                className={`${arimo.className} leading-normal tracking-normal text-2xl font-bold text-neutral-300 sm:text-4xl`}
+              >
+                What I Learned
+              </h5>
+              <p
+                className={`${nunito.className} mt-4 text-base leading-normal tracking-normal text-neutral-400`}
+              >
+                {project.lessons}
+              </p>
+            </li>
           )}
-          {project.code && (
-            <Link
-              href={project.code}
-              target="_blank"
-              className="transition ease-in-out delay-75 duration-300 text-neutral-500 hover:text-teal-500 hover:scale-105 hover:-translate-y-1 active:scale-95"
-            >
-              <FaGithub />
-            </Link>
+          {project.challenges && (
+            <li>
+              <h5
+                className={`${arimo.className} leading-normal tracking-normal text-2xl font-bold text-neutral-300 sm:text-4xl`}
+              >
+                Challenges I Faced
+              </h5>
+              <p
+                className={`${nunito.className} mt-4 text-base leading-normal tracking-normal text-neutral-400`}
+              >
+                {project.challenges}
+              </p>
+            </li>
           )}
-        </div>
-      </div>
-      <Image
-        src={project.img}
-        alt={`${project.title} project image`}
-        className="mt-4 h-auto w-full rounded"
-      />
-
-      <h2
-        className={`${poppins.className} mt-6 text-3xl/10 font-semibold bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-4xl/relaxed`}
-      >
-        {project.title}
-      </h2>
-      <p className={`${noto_sans.className} mt-4 text-lg/8 text-neutral-400`}>
-        {project.desc}
-      </p>
-      <ul className="mt-12 flex flex-col gap-y-12">
-        {project.lessons && (
-          <li>
-            <h3
-              className={`${poppins.className} text-3xl font-semibold bg-gradient-to-r from-neutral-400 to-neutral-500 inline-block text-transparent leading-noraml bg-clip-text sm:text-4xl/relaxed`}
-            >
-              What I Learned
-            </h3>
-            <p
-              className={`${noto_sans.className} mt-4 text-lg text-neutral-400`}
-            >
-              {project.lessons}
-            </p>
-          </li>
-        )}
-        {project.challenges && (
-          <li>
-            <h3
-              className={`${poppins.className} text-3xl font-semibold bg-gradient-to-r from-neutral-400 to-neutral-500 inline-block text-transparent bg-clip-text sm:text-4xl/relaxed`}
-            >
-              Challenges I Faced
-            </h3>
-            <p
-              className={`${noto_sans.className} mt-4 text-lg text-neutral-400`}
-            >
-              {project.challenges}
-            </p>
-          </li>
-        )}
-      </ul>
-    </main>
+        </ul>
+      </section>
+    </article>
   );
 }
