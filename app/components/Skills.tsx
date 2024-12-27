23188ff7c9550115589604703db1ed92062c6df8
flowz0 <billylflowers@gmail.com>
2024-12-27 02:29:06 -0800

    Improved Skills.tsx

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -7,19 +7,25 @@ import {
   TbBrandMongodb,
 } from "react-icons/tb";
 import Skill from "./Skill";
-import { noto_sans, poppins } from "../fonts";
+import { arimo, nunito } from "../fonts";
 
 export default function Skills() {
   return (
     <section
       id="skills"
-      className="py-12 rounded-lg flex flex-col items-center sm:py-16"
+      className="py-12 rounded-lg flex flex-col items-center sm:py-24"
     >
-      <h2 className={`${noto_sans.className} text-sm font-light bg-gradient-to-r from-cyan-500 to-teal-400 inline-block text-transparent bg-clip-text`}>Skills</h2>
-      <h3 className={`${poppins.className} mt-4 text-4xl font-semibold bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-5xl`}>
+      <h2
+        className={`${nunito.className} text-base leading-normal tracking-normal text-teal-400 sm:text-lg`}
+      >
+        <small>Skills</small>
+      </h2>
+      <h3
+        className={`${arimo.className} mt-2 leading-normal tracking-normal text-4xl font-bold sm:text-5xl`}
+      >
         Tech Stack
       </h3>
-      <div className="mt-8 grid grid-cols-2 gap-3 sm:gap-2.5 sm:grid-cols-4">
+      <div className="mt-8 grid grid-cols-2 gap-3 sm:gap-3.5 sm:grid-cols-4">
         <Skill icon={TbBrandMongodb} name={"MongoDB"} />
         <Skill icon={FaServer} name={"Express.js"} />
         <Skill icon={TbBrandNodejs} name={"Node.js"} />
