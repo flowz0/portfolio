e36fc4c424ad3dc9d6b3645039c9e72b28e369f8
flowz0 <billylflowers@gmail.com>
2024-12-27 02:25:47 -0800

    Update not-found.tsx (projects)

--- app/projects/not-found.tsx
+++ app/projects/not-found.tsx
@@ -1,25 +1,26 @@
 import Link from "next/link";
-import { noto_sans } from "../fonts";
-import Button from "../components/Button";
+import Button from "@/app/components/Button";
+import { arimo, nunito } from "@/app/fonts";
 
 export default function ProjectNotFound() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-[calc(100vh-160px)] px-6 text-center bg-neutral-950">
-      <h3 className="text-4xl font-semibold bg-gradient-to-r from-cyan-500 to-teal-400 inline-block text-transparent bg-clip-text sm:text-6xl">404</h3>
+    <article className="px-6 mt-12 py-12 flex flex-col items-center">
       <h2
-        className={`${noto_sans.className} mt-2 text-4xl bg-gradient-to-r from-neutral-300 to-neutral-400 inline-block text-transparent bg-clip-text sm:text-5xl/normal`}
+        className={`${nunito.className} text-base leading-normal tracking-normal text-teal-400 sm:text-lg`}
       >
-        Project Not Found
+        404
       </h2>
-      <p className="mt-4 text-neutral-400">
+      <h3
+        className={`${arimo.className} mt-2 leading-normal tracking-normal text-4xl font-bold sm:text-5xl`}
+      >
+        Project not found
+      </h3>
+      <p className={`${nunito.className} mt-8 text-base leading-normal tracking-normal text-neutral-400`}>
         Sorry, the project you&apos;re looking for doesn&apos;t exist.
       </p>
-      <Link
-        href="/projects"
-        className="mt-12 px-6"
-      >
+      <Link href="/projects" className="mt-16 px-6">
         <Button text="Back to projects" variant="animated" />
       </Link>
-    </main>
+    </article>
   );
 }
